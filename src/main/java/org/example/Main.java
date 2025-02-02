893515a512b34bde6e02c0166204f8596a5b7a65
IlyaYakubski <yakubski.ilya@student.ehu.lt>
2025-02-02 18:17:04 +0200

    Электрофрезеровщик

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,11 @@
 package org.example;
 
+import javax.sound.sampled.*;
+import java.io.File;
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
     }
 }
--- src/test/java/api/ApiTests.java
+++ src/test/java/api/ApiTests.java
@@ -1,2 +1,71 @@
-package api;public class ApiTests {
+package api;
+
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+public class ApiTests {
+
+    @BeforeAll
+    public static void setup() {
+        RestAssured.baseURI = "https://jsonplaceholder.typicode.com";
+    }
+
+    @Test
+    public void testGetCommentsByPostId_Positive() {
+        given()
+                .queryParam("postId", 1)
+                .when()
+                .get("/comments")
+                .then()
+                .statusCode(200)
+                .body("size()", greaterThan(0))
+                .body("[0].postId", equalTo(1))
+                .body("[0].email", not(emptyOrNullString()))
+                .body("[0].name", not(emptyOrNullString()));
+    }
+
+    @Test
+    public void testGetCommentsWithInvalidPostId() {
+        given()
+                .queryParam("postId", 9999)
+                .when()
+                .get("/comments")
+                .then()
+                .statusCode(200)
+                .body("size()", equalTo(0));
+    }
+
+    @Test
+    public void testGetCommentsWithoutPostId() {
+        given()
+                .when()
+                .get("/comments")
+                .then()
+                .statusCode(200)
+                .body("size()", greaterThan(10));
+    }
+
+    @Test
+    public void testGetCommentsWithInvalidPostIdType() {
+        given()
+                .queryParam("postId", "abc")
+                .when()
+                .get("/comments")
+                .then()
+                .statusCode(200)
+                .body("size()", equalTo(0));
+    }
+
+    @Test
+    public void testInvalidCommentsEndpoint() {
+        given()
+                .when()
+                .get("/wrong-comments-endpoint")
+                .then()
+                .statusCode(404);
+    }
 }
